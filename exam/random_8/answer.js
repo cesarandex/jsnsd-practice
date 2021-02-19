5030d7447cc48b9cd0347bf4fe6bea64f4b728ad
César Expósito <cexposif@everis.com>
2021-02-19 11:35:30 +0100

    Hash login exercise

--- exam/random_8/answer.js
+++ exam/random_8/answer.js
@@ -1,9 +1,66 @@
 'use strict'
 
+// FOR DEVELOPMENT
+process.env.PORT = 3012;
+process.env.HASH = "$2b$10$3JPcfQzLyyf.dOQ3b6bm8eppDPYg1zMvVurTqGRv1CeILuNmBhQN6";
+process.env.SALT = "00fc1f5ddf";
+
 const {
   PORT, HASH
-} = process.env
+} = process.env;
 
-function login (user, passwd) {
-  return user === 'node' && passwd === 'developer'
+const express = require('express');
+const bodyParser = require('body-parser');
+const bcrypt = require('bcrypt');
+
+// HASH generator, this is only needed because we don't have access to the provided environment variable HASH
+// const saltRounds = 10;
+// const myPlaintextPassword = 'developer';
+
+// bcrypt.genSalt(saltRounds, function(err, salt) {
+//   bcrypt.hash(myPlaintextPassword, salt, function(err, hash) {
+//     console.log('The generated HASH is:', hash)
+//   });
+// });
+
+const app = express();
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+app.use(bodyParser.json())
+
+async function login (user, passwd) {
+  if (user === 'node') {
+    const result = await bcrypt.compare(passwd, HASH);
+    return result;
+  }
+  return false;
 }
+
+const controller = {
+  getLogin: async (req, res) => {
+    if (req.body) {
+      const { username, password } = req.body;
+      if (username && password) {
+        const result = await login(username, password);
+        if (result) {
+          res.sendStatus(200);
+        } else {
+          res.sendStatus(401);
+        }
+        return;
+      }
+    }
+    res.status(400).send('Invalid provided body');
+  }
+};
+
+app.post('/login', controller.getLogin);
+
+app.listen(PORT, () => {
+  console.log(`[HASHED LOGIN] Listening on port ${PORT}`)
+}).on('error', () => {
+  console.log(`[HASHED LOGIN] There was an error setting up the listener`)
+});
